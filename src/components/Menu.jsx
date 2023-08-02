42d325f356224a581b4420551cc8655cbd94b6f1
Soumitra Saha <soumitrosahaofficial@gmail.com>
2023-08-02 13:41:19 +0530

    Update 1

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -18,7 +18,7 @@ const Menu = () => {
 	}, []);
 
 	return (
-		<main>
+		<main id='main'>
 			{/* Menu */}
 			<div>
 				<h1 className='main-heading'>Our Menu</h1>
@@ -26,13 +26,19 @@ const Menu = () => {
 			</div>
 
 			{/* Item List */}
-			<section>
-				<ul className='menu'>
-					<li onClick={(e) => categoryFinder()}>All</li>
-					<li onClick={(e) => categoryFinder("breakfast")}>Breakfast</li>
-					<li onClick={(e) => categoryFinder("lunch")}>Lunch</li>
-					<li onClick={(e) => categoryFinder("shakes")}>Shakes</li>
-				</ul>
+			<section className='menu'>
+				<li id='filter-btn-0' onClick={(e) => categoryFinder()}>
+					All
+				</li>
+				<li id='filter-btn-1' onClick={(e) => categoryFinder("breakfast")}>
+					Breakfast
+				</li>
+				<li id='filter-btn-2' onClick={(e) => categoryFinder("lunch")}>
+					Lunch
+				</li>
+				<li id='filter-btn-3' onClick={(e) => categoryFinder("shakes")}>
+					Shakes
+				</li>
 			</section>
 
 			{/* Items */}
